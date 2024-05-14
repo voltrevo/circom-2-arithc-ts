3ede6e676d12cfcfd4a6c789e987433289c52149
Andrew Morris <voltrevo@gmail.com>
2024-05-14 11:11:20 +1000

    Pass files to compile

--- manualTest.ts
+++ manualTest.ts
@@ -1,23 +1,25 @@
 import { initWasmLib } from "./src/wasmLib";
 
 async function main() {
-  const src = `
-    pragma circom 2.0.0;
+  const files = {
+    '/src/main.circom': `
+      pragma circom 2.0.0;
 
-    // Two element sum
-    template sum () {
-      signal input a;
-      signal input b;
-      signal output out;
-      
-      out <== a + b;
-    }
-    
-    component main = sum();
-  `;
+      // Two element sum
+      template sum () {
+        signal input a;
+        signal input b;
+        signal output out;
+
+        out <== a + b;
+      }
+
+      component main = sum();
+    `
+  };
 
   const wasmLib = await initWasmLib();
-  const circuit = wasmLib.compile(src);
+  const circuit = wasmLib.compile(files);
 
   console.log(circuit);
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,25 +1,53 @@
-use circom_2_arithc::{circom::input::Input, program::{build_circuit, ProgramError}};
+use std::collections::HashMap;
+
+use circom_2_arithc::{
+    circom::input::Input,
+    circuit::ArithmeticCircuit,
+    program::build_circuit,
+};
 use circom_virtual_fs::{FileSystem, MemoryFs};
-use wasm_bindgen::prelude::*;
-use web_sys::js_sys::Error;
+use wasm_bindgen::{prelude::*, JsError};
+use web_sys::js_sys::{Array, Object, Reflect};
 
 #[wasm_bindgen]
-pub fn compile(main: &str) -> Result<String, JsValue> {
-    compile_impl(main).map_err(to_js_err)
+pub fn compile(files: JsValue) -> Result<String, JsError> {
+    Ok(serde_json::to_string(&compile_impl(files)?)?)
 }
 
-fn to_js_err<T: std::fmt::Display>(e: T) -> JsValue {
-    Error::new(&e.to_string()).into()
+fn compile_impl(files: JsValue) -> Result<ArithmeticCircuit, JsError> {
+    let mut fs = MemoryFs::new("/src".into());
+
+    for (name, content) in convert_jsvalue_to_hashmap(files)? {
+        fs.write(&name.into(), content.as_bytes())?;
+    }
+
+    let circuit = build_circuit(&mut fs, &Input::new("/src/main.circom", "/output", None))?;
+
+    Ok(circuit)
 }
 
-fn compile_impl(main: &str) -> Result<String, ProgramError> {
-    let mut fs = MemoryFs::new("/home".into());
-    fs.write(&"/src/main.circom".into(), main.as_bytes())?;
+pub fn convert_jsvalue_to_hashmap(value: JsValue) -> Result<HashMap<String, String>, JsError> {
+    let object = value
+        .dyn_into::<Object>()
+        .map_err(|_| JsError::new("Input is not a valid object"))?;
 
-    let circuit = build_circuit(
-        &mut fs,
-        &Input::new("/src/main.circom", "/output", None),
-    )?;
+    let keys = Object::keys(&object);
+    let mut map = HashMap::new();
 
-    Ok(serde_json::to_string(&circuit)?)
+    for key in Array::from(&keys).iter() {
+        let key_str = key.as_string().ok_or(JsError::new("Key is not a string"))?;
+        let value = Reflect::get(&object, &key).map_err(|e| {
+            JsError::new(&format!(
+                "Error accessing property: {}",
+                e.as_string().unwrap_or_default()
+            ))
+        })?;
+        let value_str = value
+            .as_string()
+            .ok_or(JsError::new("Value is not a string"))?;
+
+        map.insert(key_str, value_str);
+    }
+
+    Ok(map)
 }
