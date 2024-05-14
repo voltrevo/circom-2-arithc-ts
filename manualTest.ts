759ed795773676860fa4b639284cbaa9023342d4
Andrew Morris <voltrevo@gmail.com>
2024-05-14 10:21:55 +1000

    Improve error handling

--- manualTest.ts
+++ manualTest.ts
@@ -1,26 +1,25 @@
 import { initWasmLib } from "./src/wasmLib";
 
 async function main() {
-    const src = `
-        pragma circom 2.0.0;
+  const src = `
+    pragma circom 2.0.0;
 
-        // Two element sum
-        template sum () {
-            signal input a;
-            signal input b;
-            signal output out;
-            
-            out <== a + b;
-        }
-        
-        component main = sum();
-    `;
+    // Two element sum
+    template sum () {
+      signal input a;
+      signal input b;
+      signal output out;
+      
+      out <== a + b;
+    }
+    
+    component main = sum();
+  `;
 
-    const wasmLib = await initWasmLib();
+  const wasmLib = await initWasmLib();
+  const circuit = wasmLib.compile(src);
 
-    const circuit = wasmLib.compile(src);
-
-    console.log(circuit);
+  console.log(circuit);
 }
 
 main().catch(console.error);
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,21 +1,25 @@
-use circom_2_arithc::{circom::input::Input, program::build_circuit};
-use circom_virtual_fs::{FileSystem, FsResult, MemoryFs};
+use circom_2_arithc::{circom::input::Input, program::{build_circuit, ProgramError}};
+use circom_virtual_fs::{FileSystem, MemoryFs};
 use wasm_bindgen::prelude::*;
+use web_sys::js_sys::Error;
 
 #[wasm_bindgen]
-pub fn compile(main: &str) -> String {
-    match compile_impl(main) {
-        Ok(result) => result,
-        Err(e) => e.to_string(),
-    }
+pub fn compile(main: &str) -> Result<String, JsValue> {
+    compile_impl(main).map_err(to_js_err)
 }
 
-fn compile_impl(main: &str) -> FsResult<String> {
+fn to_js_err<T: std::fmt::Display>(e: T) -> JsValue {
+    Error::new(&e.to_string()).into()
+}
+
+fn compile_impl(main: &str) -> Result<String, ProgramError> {
     let mut fs = MemoryFs::new("/home".into());
     fs.write(&"/src/main.circom".into(), main.as_bytes())?;
 
-    Ok(match build_circuit(&mut fs, &Input::new("/src/main.circom", "/output", None)) {
-        Ok(circuit) => serde_json::to_string(&circuit).unwrap(),
-        Err(e) => e.to_string(),
-    })
+    let circuit = build_circuit(
+        &mut fs,
+        &Input::new("/src/main.circom", "/output", None),
+    )?;
+
+    Ok(serde_json::to_string(&circuit)?)
 }
